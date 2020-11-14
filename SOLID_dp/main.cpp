92645fba0e480365501574bf82d9d03739d3f807
Sabrina Lokman <35489261+sabrinalokman@users.noreply.github.com>
2020-11-13 22:50:55 -0500

    first file

---
+++ SOLID_dp/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <boost/lexical_cast.hpp>
+using namespace std;
+using namespace boost;
+
+struct  Journal
+{
+	string title;
+	vector <string> entries;
+	Journal(const string &title) : title(title) {}
+
+	void add_entry(const string& entry) {
+		static int count = 1;
+		entries.push_back(lexical_cast<string>(count++) + ": " + entry);
+	}
+	
+};
+
+struct PersistenceManager
+{
+	static void save(const Journal& j, const string& filename) {
+		ofstream ofs(filename);
+		for (auto& e : j.entries)
+			ofs << e << endl;
+	}
+};
+
+int main() {
+	Journal journal{"Dear Diary"};
+	journal.add_entry("I ate a bug");
+	journal.add_entry("I cried today");
+
+	PersistenceManager pm;
+	pm.save(journal, "diary.txt");
+
+	getchar();
+	return 0;
+}
