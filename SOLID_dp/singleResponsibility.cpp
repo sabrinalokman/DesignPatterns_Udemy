76eb71ceaf272303f6aad81779d8585bab109aba
Sabrina Lokman <35489261+sabrinalokman@users.noreply.github.com>
2020-11-14 10:43:04 -0500

    Clean up

--- SOLID_dp/singleResponsibility.cpp
+++ SOLID_dp/singleResponsibility.cpp
@@ -4,11 +4,8 @@
 #include <vector>
 #include <fstream>
 #include <boost/lexical_cast.hpp>
-
 /*
-Run with 
-g++ -I /usr/local/boost/1.74.0/include main.cpp -o main && ./main
-
+Run with g++ -I /usr/local/boost/1.74.0/include 
 */
 
 using namespace std;
@@ -18,32 +15,34 @@ struct  Journal
 {
 	string title;
 	vector <string> entries;
-	Journal(const string &title) : title(title) {}
+	
+	explicit Journal(const string& title) 
+	: title(title) {}
 
 	void add_entry(const string& entry) {
 		static int count = 1;
 		entries.push_back(lexical_cast<string>(count++) + ": " + entry);
 	}
-	
 };
 
 struct PersistenceManager
 {
 	static void save(const Journal& j, const string& filename) {
 		ofstream ofs(filename);
-		for (auto& e : j.entries)
+		ofs << j.title << endl;
+		for (auto& e : j.entries) 
 			ofs << e << endl;
 	}
 };
 
 int main() {
 	Journal journal{"Dear Diary"};
-	journal.add_entry("I ate a bug");
-	journal.add_entry("I cried today");
+	journal.add_entry("I'm learning Design Patterns today");
+	journal.add_entry("Specifically Single Responsibility Pattern");
 
 	PersistenceManager pm;
-	pm.save(journal, "diary.txt");
+	pm.save(journal, "singleRes.txt");
 
-	getchar();
+	//getchar();
 	return 0;
 }
